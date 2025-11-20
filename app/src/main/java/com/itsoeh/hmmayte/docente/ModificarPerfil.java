eb4e29fe64840f6c5dacb245812e6ff8be0dbcc2
May <22011509@itsoeh.edu.mx>
2025-11-20 09:35:46 -0600

    Mod Agregar y Modificar Docente

--- app/src/main/java/com/itsoeh/hmmayte/docente/ModificarPerfil.java
+++ app/src/main/java/com/itsoeh/hmmayte/docente/ModificarPerfil.java
@@ -2,46 +2,56 @@ package com.itsoeh.hmmayte.docente;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ModificarPerfil#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.android.material.textfield.TextInputEditText;
+import com.itsoeh.hmmayte.docente.conexion.API;
+import com.itsoeh.hmmayte.docente.conexion.VolleySingleton;
+import com.itsoeh.hmmayte.docente.modelo.MDocente;
+import com.itsoeh.hmmayte.docente.util.Dialogo;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ModificarPerfil extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private TextInputEditText txtNombre;
+    private TextInputEditText txtNumero;
+    private TextInputEditText txtApp;
+    private TextInputEditText txtApm;
+    private TextInputEditText txtCorreo;
+    private Spinner spGenero;
+    private Spinner spEstado;
+    private Spinner spGrado;
+    private CardView crvActualizar;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Bundle paquete;
+    private int idDocente;
 
-    public ModificarPerfil() {
-        // Required empty public constructor
-    }
+    public ModificarPerfil() { }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ModificarPerfil.
-     */
-    // TODO: Rename and change types and number of parameters
     public static ModificarPerfil newInstance(String param1, String param2) {
         ModificarPerfil fragment = new ModificarPerfil();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString("param1", param1);
+        args.putString("param2", param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +59,179 @@ public class ModificarPerfil extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_modificar_perfil, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        vinculadores(view);
+        configurarSpinners();
+        cargarPaquete();
+        escucharBoton();
+    }
+
+    private void vinculadores(View view) {
+        txtNumero = view.findViewById(R.id.perfil_docente_txtNumero);
+        txtNombre = view.findViewById(R.id.perfil_docente_txtNombre);
+        txtApp = view.findViewById(R.id.perfil_docente_txtApPaterno);
+        txtApm = view.findViewById(R.id.perfil_docente_txtApMaterno);
+        txtCorreo = view.findViewById(R.id.perfil_usuario_txtCorreo);
+
+        spGenero = view.findViewById(R.id.sp_Genero);
+        spEstado = view.findViewById(R.id.sp_Estado);
+        spGrado = view.findViewById(R.id.sp_Grado);
+
+        crvActualizar = view.findViewById(R.id.login_btnentrar); // corregido al ID del XML
+    }
+
+    private void configurarSpinners() {
+        String[] generos = getResources().getStringArray(R.array.genero);
+        ArrayAdapter<String> adapterGeneros = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, generos);
+        adapterGeneros.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spGenero.setAdapter(adapterGeneros);
+
+        String[] estados = getResources().getStringArray(R.array.estado);
+        ArrayAdapter<String> adapterEstados = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, estados);
+        adapterEstados.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spEstado.setAdapter(adapterEstados);
+
+        String[] grados = getResources().getStringArray(R.array.grado);
+        ArrayAdapter<String> adapterGrados = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, grados);
+        adapterGrados.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spGrado.setAdapter(adapterGrados);
+    }
+
+    private void cargarPaquete() {
+        paquete = getArguments();
+        if (paquete != null) {
+            String correo = paquete.getString("correo");
+            cargarModelo(correo);
+        }
+    }
+
+    private void escucharBoton() {
+        crvActualizar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clicActualizar();
+            }
+        });
+    }
+
+    private void cargarModelo(String correo) {
+        Dialogo dialogo = new Dialogo(getContext());
+        dialogo.mostrarDialogoProgress("Por favor espere", "Conectando con el servidor");
+
+        RequestQueue queue = VolleySingleton.getInstance(getContext()).getRequestQueue();
+        StringRequest request = new StringRequest(Request.Method.POST, API.DOC_BUSCAR_POR_CORREO,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        dialogo.cerrarDialogo();
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            JSONArray array = obj.getJSONArray("msg");
+                            JSONObject reg0 = array.getJSONObject(0);
+
+                            MDocente modelo = new MDocente();
+                            idDocente = reg0.getInt("id_docente");
+                            modelo.setId_docente(idDocente);
+                            modelo.setNumero(reg0.getString("numero"));
+                            modelo.setNombre(reg0.getString("nombre"));
+                            modelo.setApp(reg0.getString("app"));
+                            modelo.setApm(reg0.getString("apm"));
+                            modelo.setCorreo(reg0.getString("correo"));
+                            modelo.setEstado(reg0.getInt("estado"));
+                            modelo.setGenero(reg0.getInt("genero"));
+                            modelo.setGrado(reg0.getInt("grado"));
+
+                            mostrarDatos(modelo);
+                        } catch (Exception ex) {
+                            dialogo.mostrarDialogoBoton("", "Error de formato: " + ex.getMessage());
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        dialogo.cerrarDialogo();
+                        dialogo.mostrarDialogoBoton("No se pudo conectar", "Verifique su conexión a Internet");
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("correo", correo);
+                return params;
+            }
+        };
+        queue.add(request);
+    }
+
+    private void mostrarDatos(MDocente modelo) {
+        txtNumero.setText(modelo.getNumero());
+        txtNombre.setText(modelo.getNombre());
+        txtApp.setText(modelo.getApp());
+        txtApm.setText(modelo.getApm());
+        txtCorreo.setText(modelo.getCorreo());
+
+        // Colocar la selección correcta en los spinners
+        spGenero.setSelection(modelo.getGenero());
+        spEstado.setSelection(modelo.getEstado());
+        spGrado.setSelection(modelo.getGrado());
+    }
+
+    private void clicActualizar() {
+        Dialogo dialogo = new Dialogo(getContext());
+        dialogo.mostrarDialogoProgress("Por favor espere", "Conectando con el servidor");
+
+        RequestQueue queue = VolleySingleton.getInstance(getContext()).getRequestQueue();
+        StringRequest request = new StringRequest(Request.Method.POST, API.DOC_ACTUALIZAR,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        dialogo.cerrarDialogo();
+                        try {
+                            JSONObject obj = new JSONObject(response);
+                            String op = obj.getString("msg");
+                            if (op.equals("true")) {
+                                dialogo.mostrarDialogoBoton("Aviso", "Datos actualizados");
+                            } else {
+                                dialogo.mostrarDialogoBoton("Error", "No se ha registrado");
+                            }
+                        } catch (Exception ex) {
+                            dialogo.mostrarDialogoBoton("", "DATOS ACTUALIZADOS");
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        dialogo.cerrarDialogo();
+                        dialogo.mostrarDialogoBoton("No se pudo conectar", "Verifique su conexión a Internet");
+                    }
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("id_docente", String.valueOf(idDocente));
+                params.put("numero", txtNumero.getText().toString());
+                params.put("nombre", txtNombre.getText().toString());
+                params.put("app", txtApp.getText().toString());
+                params.put("apm", txtApm.getText().toString());
+                params.put("correo", txtCorreo.getText().toString());
+                params.put("estado", String.valueOf(spEstado.getSelectedItemPosition()));
+                params.put("genero", String.valueOf(spGenero.getSelectedItemPosition()));
+                params.put("grado", String.valueOf(spGrado.getSelectedItemPosition()));
+                return params;
+            }
+        };
+        queue.add(request);
     }
 }
