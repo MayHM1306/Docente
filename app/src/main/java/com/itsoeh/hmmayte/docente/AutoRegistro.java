4018a90357793082c5534ba8b2b9ce2d897cf57b
May <22011509@itsoeh.edu.mx>
2025-11-20 02:54:46 -0600

    auto registro

--- app/src/main/java/com/itsoeh/hmmayte/docente/AutoRegistro.java
+++ app/src/main/java/com/itsoeh/hmmayte/docente/AutoRegistro.java
@@ -1,7 +1,6 @@
 package com.itsoeh.hmmayte.docente;
 
 import android.os.Bundle;
-
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.cardview.widget.CardView;
@@ -13,7 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.widget.Spinner;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -30,178 +29,123 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link AutoRegistro#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class AutoRegistro extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
     private NavController controladorDeNavegacion;
-    private TextInputEditText txtNumero;
-    private TextInputEditText txtNombre;
-    private TextInputEditText txtApp;
-    private TextInputEditText txtApm;
-    private TextInputEditText txtCorreo;
-    private TextInputEditText txtPass;
-    private AutoCompleteTextView txtEstado;
-    private AutoCompleteTextView txtGenero;
-    private AutoCompleteTextView txtGrado;
+
+    private TextInputEditText txtNumero, txtNombre, txtApp, txtApm, txtCorreo, txtPass;
+    private Spinner spEstado, spGenero, spGrado;
+
     private CardView crvGuardar;
-    private Bundle paquete;
-    private int idDocente;
 
-    public AutoRegistro() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AutoRegistroo.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AutoRegistro newInstance(String param1, String param2) {
-        AutoRegistro fragment = new AutoRegistro();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public AutoRegistro() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_auto_registro, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        vincularComponentes(view);
-        escuchadores(view);
 
+        vincularComponentes(view);
+        configurarListasDesplegables();
+        escuchadores();
     }
 
     private void vincularComponentes(View view) {
+
         controladorDeNavegacion = Navigation.findNavController(view);
-        txtNumero=view.findViewById(R.id.reg_numero);
-        txtNombre=view.findViewById(R.id.reg_nombre);
-        txtApp=view.findViewById(R.id.reg_app);
-        txtApm=view.findViewById(R.id.reg_apm);
-        txtCorreo=view.findViewById(R.id.reg_correo);
-        txtPass=view.findViewById(R.id.reg_pass);
-        txtEstado=view.findViewById(R.id.reg_estado);
-        txtGenero=view.findViewById(R.id.reg_genero);
-        txtGrado=view.findViewById(R.id.reg_grado);
-        crvGuardar=view.findViewById(R.id.reg_btnguardar);
+
+        txtNumero = view.findViewById(R.id.reg_numero);
+        txtNombre = view.findViewById(R.id.reg_nombre);
+        txtApp    = view.findViewById(R.id.reg_app);
+        txtApm    = view.findViewById(R.id.reg_apm);
+        txtCorreo = view.findViewById(R.id.reg_correo);
+        txtPass   = view.findViewById(R.id.reg_pass);
+
+        // ❗ AHORA sí se vinculan las variables GLOBALES (¡antes estabas creando nuevas!)
+        spEstado = view.findViewById(R.id.sp_Estado);
+        spGenero = view.findViewById(R.id.sp_Genero);
+        spGrado  = view.findViewById(R.id.sp_Grado);
+
+        crvGuardar = view.findViewById(R.id.reg_btnguardar);
     }
 
-    private void escuchadores(View view) {
-        crvGuardar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                clicGuardar(view);
-            }
-        });
+    // 🔽 CONFIGURACIÓN DE ADAPTADORES PARA SPINNERS
+    private void configurarListasDesplegables() {
 
-       String[] generos = getResources().getStringArray(R.array.genero);
-        ArrayAdapter<String> adapterGeneros = new ArrayAdapter<>(
-                view.getContext(),
-                android.R.layout.select_dialog_item,
-                generos
-        );
-        txtGenero.setAdapter(adapterGeneros);
-        String[] grados = getResources().getStringArray(R.array.grado);
-        ArrayAdapter<String> adapterGrados = new ArrayAdapter<>(
-                view.getContext(),
-                android.R.layout.select_dialog_item,
-                grados
-        );
-        txtGrado.setAdapter(adapterGrados);
-        String[] estados = getResources().getStringArray(R.array.estado);
-        ArrayAdapter<String> adapterEstados = new ArrayAdapter<>(
-                view.getContext(),
-                android.R.layout.select_dialog_item,
-                estados
-        );
-        txtEstado.setAdapter(adapterEstados);
+        ArrayAdapter<CharSequence> adapterEstado =
+                ArrayAdapter.createFromResource(requireContext(),
+                        R.array.estado, android.R.layout.simple_spinner_dropdown_item);
+        spEstado.setAdapter(adapterEstado);
+
+        ArrayAdapter<CharSequence> adapterGenero =
+                ArrayAdapter.createFromResource(requireContext(),
+                        R.array.genero, android.R.layout.simple_spinner_dropdown_item);
+        spGenero.setAdapter(adapterGenero);
+
+        ArrayAdapter<CharSequence> adapterGrado =
+                ArrayAdapter.createFromResource(requireContext(),
+                        R.array.grado, android.R.layout.simple_spinner_dropdown_item);
+        spGrado.setAdapter(adapterGrado);
     }
 
-    private void clicGuardar(View view) {
-        //validarVacios();
-        Dialogo nuevo=new Dialogo(this.getContext());//crear un cuadro de dialogo
-        nuevo.mostrarDialogoProgress("Por favor espere","Conectando con el servidor");
-        RequestQueue colaDeSolicitudes= VolleySingleton.getInstance(this.getContext()).getRequestQueue();
-        StringRequest solicitud= new StringRequest(Request.Method.POST, API.DOC_GUARDAR,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        nuevo.cerrarDialogo();//apaga el cuadro de dialogo
-                        nuevo.mostrarDialogoBoton("Aviso", response);
-                        try {
-                            //LEER AQUI EL CONTENIDO DE LA VARIABLE response
-                            JSONObject obj=new JSONObject(response);
-                            String op=obj.getString("msg");
-                            if(op.equals("true")){
-                                nuevo.mostrarDialogoBoton("Aviso","Docente registrado");
-                                controladorDeNavegacion.navigate(R.id.action_autoregistro_to_login);
-                                return;
-                            }else
-                                nuevo.mostrarDialogoBoton("Error","No se ha registrado");
+    private void escuchadores() {
+        crvGuardar.setOnClickListener(v -> clicGuardar());
+    }
 
-                        }catch (Exception ex){
-                            //DETECTA ERRORES EN LA LECTURA DEL ARCHIVO JSON
-                            nuevo.mostrarDialogoBoton("Error","Error de formato "+ex.getMessage());
+    private void clicGuardar() {
+
+        Dialogo nuevo = new Dialogo(requireContext());
+        nuevo.mostrarDialogoProgress("Por favor espere", "Conectando con el servidor");
+
+        RequestQueue colaDeSolicitudes =
+                VolleySingleton.getInstance(requireContext()).getRequestQueue();
+
+        StringRequest solicitud = new StringRequest(Request.Method.POST, API.DOC_GUARDAR,
+                response -> {
+                    nuevo.cerrarDialogo();
+                    try {
+                        JSONObject obj = new JSONObject(response);
+                        String op = obj.getString("msg");
+
+                        if (op.equals("true")) {
+                            nuevo.mostrarDialogoBoton("Aviso", "Docente registrado");
+                        } else {
+                            nuevo.mostrarDialogoBoton("Aviso", "Docente NO registrado");
                         }
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                nuevo.cerrarDialogo();
-                nuevo.mostrarDialogoBoton("No se puedo conectar","Verifique su conexion a Internet");
 
-            }
-        }){
+                    } catch (Exception ex) {
+                        nuevo.mostrarDialogoBoton("Error", "Error JSON: " + ex.getMessage());
+                    }
+                },
+                error -> {
+                    nuevo.cerrarDialogo();
+                    nuevo.mostrarDialogoBoton("No se pudo conectar",
+                            "Verifique su conexión a Internet");
+                }) {
+
             @Override
-            protected Map<String, String> getParams(){
-                Map<String, String> param=new HashMap<String,String>();
+            protected Map<String, String> getParams() {
+                Map<String, String> param = new HashMap<>();
+
                 param.put("numero", txtNumero.getText().toString());
                 param.put("nombre", txtNombre.getText().toString());
                 param.put("app", txtApp.getText().toString());
                 param.put("apm", txtApm.getText().toString());
                 param.put("correo", txtCorreo.getText().toString());
                 param.put("pass", txtPass.getText().toString());
-                param.put("estado", txtEstado.getText().toString());
-                param.put("genero", txtGenero.getText().toString());
-                param.put("grado", txtGrado.getText().toString());
+                param.put("estado", spEstado.getSelectedItem().toString());
+                param.put("genero", spGenero.getSelectedItem().toString());
+                param.put("grado", spGrado.getSelectedItem().toString());
+
                 return param;
             }
         };
+
         colaDeSolicitudes.add(solicitud);
-
-
     }
 }
--- app/src/main/java/com/itsoeh/hmmayte/docente/conexion/API.java
+++ app/src/main/java/com/itsoeh/hmmayte/docente/conexion/API.java
@@ -3,7 +3,7 @@ package com.itsoeh.hmmayte.docente.conexion;
 public interface API {
     //CONEXION A LA BASE DE DATOS
     //public String URL = "http://178.6.9.133/wsescuela/"; // IP MAYTE - CASA CAMBIALO
-    public String URL = "http://192.168.1.148/wsescuela/"; // IP BETO - CASA
+    public String URL = "http://192.168.12.103/wsescuela/"; // IP BETO - CASA
 
     //AUTENTICACION
     public String VERIFICA = URL + "apiD.php?api=validar";
@@ -15,4 +15,6 @@ public interface API {
 
     // CRUD GRUPO
     public String DOC_LISTAR_GRUPOS = URL + "apiG.php?api=listargrupos";
+    String LISTAR_INSCRITOS = URL + "apiG.php?api=listargrupos";
+    String ASISTENCIA_GUARDAR = URL + "apiG.php?api=listargrupos";
 }
